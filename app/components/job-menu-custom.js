a7de055e4fd752636c69d347da6051a9bc410525
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-05-21 14:48:23 -0400

    testing

--- app/components/job-menu-custom.js
+++ app/components/job-menu-custom.js
@@ -1,7 +1,18 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
   selectAddRoll: false,
   selectAwardBeats: false,
+
+  @action
+  setSelectAddRoll(value) {
+    this.set('selectAddRoll', value);
+  },
+
+  @action
+  setSelectAwardBeats(value) {
+    this.set('selectAwardBeats', value);
+  },
 });
